f2d2d7a2d8094e557bf04d4d4a7de2637be34dc4
MinaDakiri <minadakiri7@gmail.com>
2021-04-19 13:49:49 +0000

    Create index.js

---
+++ index.js
@@ -0,0 +1,32 @@
+
+const express = require('express');
+const moongoose = require('mongoose');
+
+// Imports Routes
+const clientRoutes = require ('./routes/clients');
+
+//Config app
+const app = express();
+require('dotenv').config();
+
+//database
+moongoose.connect(process.env.DATABASE,{
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+
+})
+.then(() => console.log('db connected'))
+.catch(() => console.log('not connected to the database!') )
+
+//Middleware
+app.use(express.json())
+
+//Routes Middleware
+app.use('/api/clients', clientRoutes);
+
+
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`app is running  on port ${port}`));
