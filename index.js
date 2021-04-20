9602f2a97134e15cdfaa27b5fc1019113ee8b7a6
MinaDakiri <minadakiri7@gmail.com>
2021-04-20 23:06:39 +0000

    Add functionalty for send emails from index.js app

--- index.js
+++ index.js
@@ -2,6 +2,38 @@
 const express = require('express');
 const moongoose = require('mongoose');
 
+//sendmailer
+var nodemailer = require('nodemailer');
+const { info } = require('./controllers/clientController');
+
+var transport = nodemailer.createTransport({
+    
+        service:'gmail',
+        auth:{
+            user: 'jsnode22@gmail.com',
+            pass:'Nodejs1234'
+        }
+    
+
+})
+
+//send out email
+
+var mailOptions = {
+    from: 'jsnode22@gmail.com',
+    to: 'minadakiri7@gmail.com',
+    subject: 'testing mail',
+    text:'This is the body of the mail'
+}
+transport.sendMail(mailOptions, function(error,info){
+    if(error){
+        console.log(error)
+    }
+    else{
+        console.log("Email send" + info.response) 
+    }
+})
+
 // Imports Routes
 const clientRoutes = require ('./routes/clients');
 
