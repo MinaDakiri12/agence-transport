57745965d4e9b916b0038a112eb78fb992f7bac0
MinaDakiri <minadakiri7@gmail.com>
2021-04-20 23:33:21 +0000

    Add functionality for send mail into clientController.js

--- controllers/clientController.js
+++ controllers/clientController.js
@@ -1,4 +1,34 @@
 const Client = require('../models/clients')
+var nodemailer = require('nodemailer');
+
+//sendmailer
+var transport = nodemailer.createTransport({
+    
+        service:'gmail',
+        auth:{
+            user: 'jsnode22@gmail.com',
+            pass:'Nodejs1234'
+        }
+    
+
+})
+
+//send out email
+
+var mailOptions = {
+    from: 'jsnode22@gmail.com',
+    to: 'minadakiri7@gmail.com',
+    subject: 'testing send mail',
+    text:'This is the body of the mail'
+}
+transport.sendMail(mailOptions, function(error,info){
+    if(error){
+        console.log(error)
+    }
+    else{
+        console.log("Email send" + info.response) 
+    }
+})
 exports.test = (req, res) => {
     res.send({message:'clients module' })
 }
