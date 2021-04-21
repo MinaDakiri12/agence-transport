9630412964c1a925ea8a34999a5b967efa4a3e29
MinaDakiri <minadakiri7@gmail.com>
2021-04-21 17:19:00 +0000

    Create index.js

---
+++ front-end/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// eslint-disable-next-line
+import Routes from './Routes';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Routes />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
