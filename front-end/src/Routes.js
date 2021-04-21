74c597aeff81a9f74007a10883840a0cd92601d2
MinaDakiri <minadakiri7@gmail.com>
2021-04-21 17:18:54 +0000

    Create Routes.js

---
+++ front-end/src/Routes.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+
+import Contact from './form/Contact';
+import Home from './core/Home';
+import Menu from './core/Menu';
+
+function Routes() {
+    return (
+       <BrowserRouter>
+          <Menu />
+          <Switch>
+              <Route path='/contact' exact component={Contact}/>
+              <Route path='/home' exact component={Home}/>
+          </Switch>
+       </BrowserRouter>
+    )
+}
+
+export default Routes
