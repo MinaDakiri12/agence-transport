a31b72d5e7e15ed81175d3c133b06f33082c04e4
MinaDakiri <minadakiri7@gmail.com>
2021-04-24 15:07:21 +0000

    package

--- routes/clients.js
+++ routes/clients.js
@@ -1,11 +1,17 @@
 const express = require('express');
-const {client, info} = require('../controllers/clientController');
-
 const router = express.Router();
+const {addClient, info,  replyClient, singleClient, findClient} = require('../controllers/clientController');
 
 
 
-router.post('/client', client);
 router.get('/info', info);
+router.post('/client', addClient);
+router.post('/reply/:id', replyClient);
+router.post('/search', findClient);
+//router.post('/singleclient/:id', singleClient)
+
+
+
+
 
 module.exports = router;
