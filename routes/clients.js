cdc18f694dc9513f7c3ac79376788f45b4ad1036
MinaDakiri <minadakiri7@gmail.com>
2021-04-24 16:44:09 +0000

    Update clients.js

--- routes/clients.js
+++ routes/clients.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const {addClient, info,  replyClient, singleClient, findClient} = require('../controllers/clientController');
+const {addClient, info,  reply, single, findClient} = require('../controllers/clientController');
 
 
 
 router.get('/info', info);
 router.post('/client', addClient);
-router.post('/reply/:id', replyClient);
+router.post('/reply/:id', reply)
 router.post('/search', findClient);
-//router.post('/singleclient/:id', singleClient)
+router.post('/single/:id', single)
 
 
 
